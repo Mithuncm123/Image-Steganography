1cb77b957d3194215ecb63cebaf8c227c8590436
Mithun C M <mithunpraju@gmail.com>
2025-09-22 18:20:48 +0530

    Add files via upload

---
+++ Image_Steganography/Stegano_Project/stegano_app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+import stepic
+from PIL import Image
+import io
+import os
+# Create your views here.
+
+def index(request):
+    return render(render,'index.html')
+
+def hide_text_in_image(image,text):
+    data=text.encode('utf-8') #text convert into bytes,utf-8 --> scheme
+    return stepic.encode(image,data)
+def encryption_view(request):
+    message=""
+    if request.method=="POST":
+        text=request.POST['text']
+        image_file=request.FILES['image']
+        image=Image.open(image_file)
+        if image.format!='PNG':
+            image=image.convert('RGBA') #RGBA--> RED,BLUE,GREEN AND ALPHA
+            buffer=io.BytesIO()
+            image.save(buffer,format="PNG")
+            image=Image.open(buffer)
+        new_image=hide_text_in_image(image,text)
+        filename = os.path.splitext(image_file.name)[0]  # remove original extension
+        image_path = os.path.join('encrypted_images', f'{filename}.png')
+        new_image.save(image_path, format='PNG') 
+        message="Text has been encrypted in the image"
+    return render(request,'encryption.html', locals())
+
+def decryption_view(request):
+    text = ""
+    message = ""
+    if request.method == 'POST':
+        image_file = request.FILES.get('image')
+        if image_file:
+            image = Image.open(image_file)
+            if image.format != 'PNG':
+                image = image.convert('RGBA')
+                buffer = io.BytesIO()
+                image.save(buffer, format="PNG")
+                image = Image.open(buffer)
+            text = extract_text_from_image(image)
+            if text:
+                message = "Text successfully decrypted!"
+            else:
+                message = "No hidden text found in this image."
+        else:
+            message = "Please upload an image."
+    
+    context = {'text': text, 'message': message}
+    return render(request, 'decryption.html', context)
+
+
+def extract_text_from_image(image):
+    data=stepic.decode(image)
+    if isinstance(data,bytes):
+        return data.decode('utf-8')
+    return data
+
